3f6019d6b3701bf8f73b00ff059d25352810069f
yarik <ayaroslav2011@gmail.com>
2022-11-27 20:10:44 +0200

    dz

---
+++ Нова папка (2)/dz4.py
@@ -0,0 +1,41 @@
+
+
+def divider(a, b):
+ if a < b:
+    raise ValueError
+ if b > 100:
+    raise IndexError
+ else:
+    return a/b
+
+data = {10: 2, 2: 5, "123": 4, 18: 0, 30 : 15, 8: 4}
+
+while True:
+    key = 0
+    result = []
+    try:
+        for key in data:
+            res = divider(key, data[key])
+            result.append(res)
+
+    except(ValueError):
+        print(f"ValueError : {key}")
+        del data[key]
+        continue
+
+    except(TypeError):
+        print(f"TypeError : {key}")
+        del data[key]
+        continue
+
+    except(ZeroDivisionError):
+        print(f"ZeroDivisionError : {key}")
+        del data[key]
+        continue
+
+
+
+    else:
+        print(result)
+        break
+   
